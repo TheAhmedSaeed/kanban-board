817a16e6bebe1b2a442c8dd57bdcc085da38cd3f
ahmmedms <mrmoood@hotmail.com>
2020-09-24 01:57:28 +0300

    added the dialogs

--- src/app/kanban/kanban.module.ts
+++ src/app/kanban/kanban.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BoardListComponent } from './board-list/board-list.component';
-import { BoardComponent } from './board/board.component';
+
 import { KanbanRoutingModule } from './kanban-routing.module';
+import { BoardListComponent } from './board-list/board-list.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BoardComponent } from './board/board.component';
+import { FormsModule } from '@angular/forms';
+import { BoardDialogComponent } from './dialogs/board-dialog.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { TaskDialogComponent } from './dialogs/task-dialog.component';
 
 @NgModule({
-  declarations: [BoardListComponent, BoardComponent],
-  imports: [CommonModule, KanbanRoutingModule, SharedModule],
+  declarations: [
+    BoardListComponent,
+    BoardComponent,
+    BoardDialogComponent,
+    TaskDialogComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    KanbanRoutingModule,
+    FormsModule,
+    DragDropModule,
+    MatDialogModule,
+    MatButtonToggleModule,
+  ],
+  entryComponents: [BoardDialogComponent, TaskDialogComponent],
 })
 export class KanbanModule {}
